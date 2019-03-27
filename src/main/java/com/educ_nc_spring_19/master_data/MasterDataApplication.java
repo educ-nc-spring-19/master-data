3e65d62756b3dc028c28d15c1234c22635925be4
Konstantin Stepanov <kamael-sp@mail.ru>
2019-03-27 12:50:30 +0300

    Initialize project

---
+++ src/main/java/com/educ_nc_spring_19/master_data/MasterDataApplication.java
@@ -0,0 +1,13 @@
+package com.educ_nc_spring_19.master_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educ_nc_spring_19/master_data/MasterDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.educ_nc_spring_19.master_data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
