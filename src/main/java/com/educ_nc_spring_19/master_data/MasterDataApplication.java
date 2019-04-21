9b81c711830dcccf50789a7c5a9ac26a0472a67a
Konstantin Stepanov <kamael-sp@mail.ru>
2019-04-21 14:40:41 +0300

    [DEV-48] Enable CORS

--- src/main/java/com/educ_nc_spring_19/master_data/MasterDataApplication.java
+++ src/main/java/com/educ_nc_spring_19/master_data/MasterDataApplication.java
@@ -2,6 +2,9 @@ package com.educ_nc_spring_19.master_data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MasterDataApplication {
@@ -10,4 +13,13 @@ public class MasterDataApplication {
         SpringApplication.run(MasterDataApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:55100").allowedMethods("*");
+            }
+        };
+    }
 }
